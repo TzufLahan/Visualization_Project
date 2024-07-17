7270d67e2a7f78ca32f77ffcd2ade4ac58070a93
Ayelet Hashahar Cohen <45938000+AyeletHashahar@users.noreply.github.com>
2024-07-17 14:58:09 +0300

    Update visualization_project.py

--- visualization_project.py
+++ visualization_project.py
@@ -216,7 +216,7 @@ if not filtered_data.empty:
                                    color='Education',
                                    title=f'Politeness by Education in {selected_region}',
                                    color_discrete_sequence=px.colors.sequential.Blues[::-1],  # Adjust color sequence for deeper colors
-                                   size_max=40,  # Adjust size_max for larger starting size
+                                   size_max=30,  # Adjust size_max for larger starting size
                                    range_y=[0, education_politeness['politeness_score_normalized'].max() * 1.4],  # Set Y-axis to start from 0
                                    height=400,  # Adjust height
                                    width=500)  # Adjust width
